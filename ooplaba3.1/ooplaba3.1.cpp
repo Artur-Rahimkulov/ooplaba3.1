769cca86f59bc6f372699ed58f8fb43e10f6bd68
Rahimkulov <q3032002@gmail.com>
2021-12-11 03:22:45 +0500

    ujnjdj

--- ooplaba3.1/ooplaba3.1.cpp
+++ ooplaba3.1/ooplaba3.1.cpp
@@ -2,7 +2,7 @@
 #include <ctime>
 using namespace std;
 
-class Item {// базовый класс
+class Item {
 public:
 	virtual void showName() {
 		printf("Item\n");
@@ -11,50 +11,50 @@ public:
 	}
 };
 
-class Point : public Item {//класс наследник от класса Thing
+class Point : public Item {
 private:
-	int x;
+	int x;  
 	int y;
 public:
 	Point() {
-		x = 0;
+		x = 0; 
 		y = 0;
 	}
 
 	Point(int x, int y) {
 		this->x = x;
-		this->y = y;
+		this->y = y; 
 	}
-
+	 
 	Point(const Point& t) {
-		x = t.x;
+		x = t.x; 
 		y = t.y;
 	}
-
-	void showName() {//метод, показывающий название класса
-		printf("Point\n");
+	 
+	void showName() {
+		printf("Point\n"); 
 	}
 	~Point() {
-	}
+	} 
 };
 
-class ColoredPoint : public Item {//другой класс наследник от класса Thing
+class ColoredPoint : public Item {
 private:
 	int x;
-	int y;
+	int y; 
 	int color;
 public:
 	ColoredPoint() {
 		x = 0;
 		y = 0;
-		color = 0;
+		color = 0; 
 	}
 
 	ColoredPoint(int x, int y, int color) {
 		this->x = x;
 		this->y = y;
 		this->color = color;
-	}
+	} 
 
 	ColoredPoint(const ColoredPoint& c) {
 		x = c.x;
@@ -63,17 +63,17 @@ public:
 	}
 	~ColoredPoint() {
 
-	}
+	} 
 
-	void showName() {//метод, показывающий название класса
+	void showName() {
 		printf("ColoredPoint\n");
 	}
 };
-
-class MyStorage {//класс хранилище
+  
+class MyStorage {
 private:
-	int size;//размер массива
-	Item** storage;//массив указателей
+	int size;
+	Item** storage;
 public:
 
 	MyStorage() {
@@ -81,55 +81,55 @@ public:
 	}
 
 	MyStorage(int i) {
-		size = i;
+		size = i; 
 		storage = new Item * [size];
 	}
 
 	void setObject(int i, Item* newObject) {
 		storage[i] = newObject;
-	}
+	} 
 
-	void addObject(Item* newObject) {//добавляет объект в массив
+	void addObject(Item* newObject) {
 		Item** storage2 = new Item * [size + 1];
 		if (size != 0) {
 			for (int j = 0; j < size; j++) {
-				storage2[j] = storage[j];
+				storage2[j] = storage[j]; 
 			}
 			delete[] storage;
 		}
 		storage = storage2;
 		storage[size] = newObject;
-		size = size + 1;
+		size = size + 1; 
 	}
 
 
 	Item& getObject(int i) {
 		return *storage[i];
-	}
+	} 
 
-	int getCount() {//возвращает размер массива
+	int getCount() {
 		return size;
 	}
 
-
+	 
 	void deleteObject(int i) {
 		storage[i] = nullptr;
 	}
 
-	void removeObject(int i) {//удаляет объект из массива
-		if (size != 0 && i < size && size != 1) {
+	void removeObject(int i) {
+		if (size != 0 && i < size && size != 1) { 
 			Item** storage2 = new Item * [size - 1];
 			for (int j = 0; j < i; j++) {
 				storage2[j] = storage[j];
 			}
 			for (int j = i; j < size - 1; j++) {
-				storage2[j] = storage[j + 1];
+				storage2[j] = storage[j + 1]; 
 			}
 			delete storage[i];
 			delete[] storage;
 			size = size - 1;
 			storage = storage2;
-		}
+		} 
 		else if (size == 1) {
 			storage[0] = nullptr;
 			size = 0;
@@ -138,52 +138,52 @@ public:
 
 
 	}
-
-	void showNameObject(int i) {//вызывает showName у определенного объекта
+	 
+	void showNameObject(int i) {
 		if (size != 0 && size > i)
 			storage[i]->showName();
 	}
 
-	void showNameStorage() {//вызывает showName у всех объектов
+	void showNameStorage() { 
 		if (size != 0) {
 			for (int i = 0; i < size; i++) {
 				storage[i]->showName();
 			}
 		}
-	}
+	} 
 
 	~MyStorage() {
 		for (int i = 0; i < size; i++) {
 			delete storage[i];
 		}
-		delete[] storage;
+		delete[] storage; 
 	}
 };
 
-void cycle(MyStorage& storage, int max) {//цикл, который позволяет повторить несколько действии 100, 1000, 10000 раз
+void cycle(MyStorage& storage, int max) {
 	unsigned int s = clock();
 	printf("\n\n");
 	for (int i = 0; i < max; i++) {
 		int a = rand() % 3;
-		int b;
+		int b; 
 		if (storage.getCount() != 0) {
 			b = rand() % storage.getCount();
 		}
 		else {
 			b = 0;
-		}
+		} 
 
 		int k = rand() % 2;
 		switch (a) {
 		case 0:
 			if (k == 0)
-				storage.addObject(new Point);
+				storage.addObject(new Point); 
 			else
 				storage.addObject(new ColoredPoint);
 
 			printf("addObject\n");
 			break;
-		case 1:
+		case 1: 
 			storage.removeObject(b);
 			printf("removeObject\n");
 			break;
